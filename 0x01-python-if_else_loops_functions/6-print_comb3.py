8e08481b8567b8b4ada616f6abefbd3420a94adc
Jed <jedbahena@gmail.com>
2022-06-01 12:57:40 -0700

    wrote script

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for a in range(0, 9):
+    for b in range(1, 10):
+
+        if b > a:
+            if a != 8 or b != 9:
+                print("{0}{1}".format(a, b), end=', ')
+            
+            else:
+                print("{0}{1}".format(a, b))
