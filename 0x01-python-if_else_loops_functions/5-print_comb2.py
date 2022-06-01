77fc6dfe24c1da82fa5fada1da1fc6e43221666f
Jed <jedbahena@gmail.com>
2022-06-01 12:12:02 -0700

    fixed the script

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 for a in range(1, 100):
 
     if a < 99:
-        print(f'0{a:d}'if a < 10 else f'{a:d}', end=', ')
+        print("0{}".format(a) if a < 10 else "{}".format(a), end=', ')
 
     else:
-        print(f'{a:d}')
+        print(a)
