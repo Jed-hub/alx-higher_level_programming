b054677bb928bd3242be7343bf20e0ded61d4f2f
Jed <jedbahena@gmail.com>
2022-06-02 07:20:10 -0700

    wrote the script

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    i = len(argv)
+
+    if i == 2:
+        print("0")
+
+    elif i > 2:
+        n = 1
+        result = 0
+        while n != i:
+            result += int(argv[i - n])
+            n += 1
+        print(result)
