88a5be9ee812dfd97262bf01443f871b17dce4fe
Jed <jedbahena@gmail.com>
2022-06-02 15:58:06 -0700

    fixed bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
+
     i = len(argv)
 
-    if i == 2:
+    if i <= 2:
         print("0")
 
     elif i > 2:
         n = 1
         result = 0
-        while n != i:
+        while n < i:
             result += int(argv[i - n])
             n += 1
         print(result)
