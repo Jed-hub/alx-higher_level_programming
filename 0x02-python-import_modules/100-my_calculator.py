ccca057f1ca3e13bc98baccd0adb4de5f75effab
Jed <jedbahena@gmail.com>
2022-06-02 10:16:34 -0700

    wrote script

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+
+    i = len(argv)
+
+    if i < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    else:
+
+        if argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+        else:
+
+            if argv[2] == "+":
+                print("{0} + {1} =".format(int(argv[1]), int(argv[3])), add(int(argv[1]), int(argv[3])))
+            elif argv[2] == "-":
+                print("{0} - {1} =".format(int(argv[1]), int(argv[3])), sub(int(argv[1]), int(argv[3])))
+            elif argv[2] == "*":
+                print("{0} * {1} =".format(int(argv[1]), int(argv[3])), mul(int(argv[1]), int(argv[3])))
+            elif argv[2] == "/":
+                print("{0} / {1} =".format(int(argv[1]), int(argv[3])), div(int(argv[1]), int(argv[3])))
