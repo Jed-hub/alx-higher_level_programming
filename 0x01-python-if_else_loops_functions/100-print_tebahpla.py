d76e27f17dbada170c47e1d9c4cc8a8226345aed
Jed <jedbahena@gmail.com>
2022-06-01 17:36:50 -0700

    fixed pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(-90, -64):
-    print("{}".format(chr(-i + 32)) if i % 2 == 0 
-            else "{}".format(chr(-i)), end='')
+    print("{}".format(chr(-i + 32)) if i % 2 == 0
+          else "{}".format(chr(-i)), end='')
