294d6e4be2281e493a57ce710ace1089a629130e
Jed <jedbahena@gmail.com>
2022-06-02 06:38:17 -0700

    wrote the script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    i = len(argv)
+
+    if i == 1:
+        print("0 arguments.")
+
+    elif i == 2:
+        print(i - 1, "argument:")
+        print("1:", argv[i - 1])
+
+    else:
+        print(i - 1, "arguments:")
+       # for a in argv:
+        n = 1
+
+        while n < i:
+            print("{}:".format(n), argv[n])
+            n += 1
