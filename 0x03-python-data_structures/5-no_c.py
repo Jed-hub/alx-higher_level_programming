64d38302358342dd1f58756a88a0d9627fbd8bba
Jed <jedbahena@gmail.com>
2022-06-06 18:03:31 -0700

    done

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for s in my_string:
+        if s == "c" or s == "C":
+            print("T")
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(1):
+            i += 1
+            for n in range(i):
+                print("{}".format(matrix[n]))
