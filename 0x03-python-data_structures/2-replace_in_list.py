b8c0359c3fe3abbc1a3a3721707b5b757fbe1f7c
Jed <jedbahena@gmail.com>
2022-06-19 16:13:41 -0700

    fixed bugs

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in my_list:
-        i += 1
     if idx < 0:
         return my_list
-    elif idx >= i - 1:
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    #new_list = my_list
-    for i in my_list:
-        i += 1
+    new_list = my_list[:]
+    new_list[idx] = element
 
-    #new_list = my_list
     if idx < 0:
         return my_list
-    elif idx > i - 1:
+    elif idx >= len(my_list):
         return my_list
     else:
-        n = my_list[idx]
-        #new_list = my_list
-        new_list[idx] = element 
-        my_list[idx] = n
-        #new_list[] = element
         return new_list
