7cf541a4227570e9ed5b79b2c7e95058b2f8f23b
Jed <jedbahena@gmail.com>
2022-06-20 11:38:15 -0700

    fixed bugs

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
 
         try:
             print("{:d}".format(my_list[n]), end="")
-        except (ValueError, TypeError):
-            x = n - 1
+        except ValueError:
             pass
+        except TypeError:
+            x = n - 1
     print("")
     return x
