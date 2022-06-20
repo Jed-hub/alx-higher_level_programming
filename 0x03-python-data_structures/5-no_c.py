08f84f0dca61e3d6abcdc017cdbf65c0d6074f73
Jed <jedbahena@gmail.com>
2022-06-19 17:13:28 -0700

    fixed bugs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    #n = 0
-    #my_list = []
     for c in my_string:
-        #print(c, end="")
-        #my_list[n] = c
-        #n += 1
         if c == "c" or c == "C":
             c == ""
         else:
             print("{}".format(c), end="")
-    #print("{}".format(my_list))
+
+    return c
