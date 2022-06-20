b3cd12e37209df4f4429e477ccbc269e657927b5
Jed <jedbahena@gmail.com>
2022-06-20 09:58:43 -0700

    created the script

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for n in range(x):
+            if n < x - 1:
+                print("{}".format(my_list[n]), end="")
+            else:
+                print("{}".format(my_list[n]))
+
+    except:
+        print("")
+        return n
+    else:
+        return x
