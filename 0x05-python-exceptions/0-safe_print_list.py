5872e5e7efc35b874f11cb0936e67b919684fd5e
Jed <jedbahena@gmail.com>
2022-06-20 10:01:08 -0700

    fixed bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print("{}".format(my_list[n]))
 
-    except:
+    except IndexError:
         print("")
         return n
     else:
