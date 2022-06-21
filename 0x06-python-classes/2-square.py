d6dc9de8beab82493dc8688d49af9f442d0208fa
Jed <jedbahena@gmail.com>
2022-06-21 16:38:55 -0700

    wrote the class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module create a class Square with a private instance attribute """
+
+
+class Square:
+    """ The class square that defines a square by size"""
+    def __init__(self, size=0):
+        """ Initialisation of the attribute size """
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
