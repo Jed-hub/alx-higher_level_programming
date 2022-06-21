6e6cbf3a45d67b5fe3674ec0ad458e3bd1d1b897
Jed <jedbahena@gmail.com>
2022-06-21 15:53:15 -0700

    added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" This module create a class Square with a private instance attribute """
+
+
 class Square:
+    """ The class square that defines a square by size"""
     def __init__(self, size):
+        """ Initialisation of the attribute size """
         self.__size = size
