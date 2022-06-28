78018b1ab6a3b4424643dbbcfdb19cc57bc53d34
Jed <jedbahena@gmail.com>
2022-06-28 12:02:15 -0700

    files

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+MAGIC STRING
+"""
+def magic_string(l=[]):
+    """
+    Returns a string n times
+    the number of itration
+    """
+    l += ["BestSchool"]
+    return ", ".join(l)
