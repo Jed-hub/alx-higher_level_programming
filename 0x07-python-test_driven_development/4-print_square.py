628a9c76e8bbba6fb63ef3fdfeafbd6f3beaefe3
Jed <jedbahena@gmail.com>
2022-06-28 16:27:20 -0700

    fixed style

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-""" This module prints a square """
+"""
+This module contain a function
+that prints a square
+with the character #
+"""
+
 
 def print_square(size):
-    """ This function prints a square withe the character # """
+    """ This function prints a square
+    with the given agrgument as size length of the square
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
