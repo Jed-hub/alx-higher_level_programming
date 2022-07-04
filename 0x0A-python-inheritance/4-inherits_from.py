ec82e773fac559275ab074389bdfc1815d9ca10a
Jed <jedbahena@gmail.com>
2022-07-04 16:51:40 -0700

    added file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The module with the function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class
+    otherwise False
+    """
+    if type(obj) is a_class:
+        return False
+    else:
+        return isinstance(obj, a_class)
