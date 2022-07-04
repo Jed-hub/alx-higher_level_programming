349bcbcaf8368727f8cf0057dd02967fd5be177f
Jed <jedbahena@gmail.com>
2022-07-04 16:12:15 -0700

    added file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Lookup module with one function
+"""
+
+def lookup(obj):
+    """ This function returns the list
+    of available attributes and methods of an object
+    """
+    return dir(obj)
