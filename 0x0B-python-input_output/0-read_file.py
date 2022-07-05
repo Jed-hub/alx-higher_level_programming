45e45b967b1fac7bbc2f43731b3ff28a876a6711
Jed <jedbahena@gmail.com>
2022-07-05 16:50:12 -0700

    added file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module with the function read_file
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file
+    and prints it to stdout
+    """
+    with open(filename, encoding="utf-8") as a_file:
+        print(a_file.read())
