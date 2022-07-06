804de295a0b024bbe8171292c8c2281d68907692
Jed <jedbahena@gmail.com>
2022-07-05 18:25:51 -0700

    fixed style

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,19 +5,17 @@ My module
 
 
 def append_after(filename="", search_string="", new_string=""):
-	"""
-        search and update
-        """
-	read = []
-	with open(filename, "r", encoding="utf-8") as f:
-		read = f.readlines()
-		idx = 0
-
-		while idx < len(read):
-			if search_string in read[idx]:
-				read[idx:idx + 1] = [read[idx], new_string]
-				idx += 1
-			idx += 1
-
-	with open(filename, "w", encoding="utf-8") as file:
-		file.writelines(read)
+    """
+    search and update
+    """
+    read = []
+    with open(filename, "r", encoding="utf-8") as f:
+        read = f.readlines()
+        idx = 0
+        while idx < len(read):
+            if search_string in read[idx]:
+                read[idx:idx + 1] = [read[idx], new_string]
+                idx += 1
+            idx += 1
+        with open(filename, "w", encoding="utf-8") as file:
+            file.writelines(read)
