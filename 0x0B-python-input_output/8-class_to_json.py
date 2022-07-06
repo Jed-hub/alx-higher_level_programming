59d31ea11d29f1d238f153c644a98d3e4dc3fc5b
Jed <jedbahena@gmail.com>
2022-07-05 17:51:49 -0700

    added file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+My class module
+"""
+
+
+def class_to_json(obj):
+    """ 
+    Return the dictionary description
+    with the simple data structure
+    """
+    return obj.__dict__
