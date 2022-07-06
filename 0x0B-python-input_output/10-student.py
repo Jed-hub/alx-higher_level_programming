e20db88f965abf0370d1e7a20af6f6b67618551c
Jed <jedbahena@gmail.com>
2022-07-05 17:56:47 -0700

    added file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Student to JSON with filter. """
+
+
+class Student:
+    """ Define a student """
+    def __init__(self, first_name, last_name, age):
+        """ Instation with first_name, last_name, age. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dictionary representation of a Student.
+        Return:
+            a dictionary representation of a Student instance
+            with specified attributes
+        """
+        if attrs is None:
+            return self.__dict__
+
+        dict_t = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dict_t[i] = self.__dict__[i]
+        return dict_t
