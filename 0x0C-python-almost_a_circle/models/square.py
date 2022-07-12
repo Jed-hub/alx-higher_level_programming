5684da589793a51aa8cbae305602888c3f0c136e
Jed <jedbahena@gmail.com>
2022-07-11 19:07:56 -0700

    added update option

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -42,3 +42,34 @@ class Square(Rectangle):
         """
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """
+        the update method assigns an argument
+        to each attribute
+        """
+        arg_list = []
+        arg_dict = {}
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                arg_dict[key] = value
+            if 'id' in arg_dict:
+                self.id = arg_dict['id']
+            if 'size' in arg_dict:
+                self.width = arg_dict['size']
+            if 'x' in arg_dict:
+                self.x = arg_dict['x']
+            if 'y' in arg_dict:
+                self.y = arg_dict['y']
+
+        for arg in args:
+            arg_list.append(arg)
+        if len(arg_list) >= 1:
+            self.id = arg_list[0]
+        if len(arg_list) == 2:
+            self.width = arg_list[1]
+        if len(arg_list) == 3:
+            self.x = arg_list[2]
+        if len(arg_list) == 4:
+            self.y = arg_list[3]
