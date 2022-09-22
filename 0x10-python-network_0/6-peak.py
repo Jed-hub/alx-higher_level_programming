c65623e3d9265852834a72095d3c2858d75b4f1b
Jed <jedbahena@gmail.com>
2022-09-22 07:29:31 -0700

    fixed pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,8 @@ def find_peak(list_of_integers):
     if length == 0:
         return
     m = length // 2
-    if (m == length - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+    if (m == length - 1 or li[m] >= li[m + 1])\
+            and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
     if m != length - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
